f8cc0d5a7b3da1cd7b30e12ea8e7fb7e3fe7cb30
Aman Soni <48171775+AmanSonii@users.noreply.github.com>
2024-09-09 08:44:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureKeyVault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyVaultUrl = "";
+            string clientId = "";
+            string clientSecret = "";
+            string tenantId = "";
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+            var client = new SecretClient(new Uri(keyVaultUrl), clientSecretCredential);
+
+            KeyVaultSecret secret = client.GetSecret("SecretNAME");
+        }
+    }
+}
